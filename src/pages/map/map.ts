4ef7529adf7d13e359603e4a68bb22f0b99be697
Yoland Gao <yoland5286@gmail.com>
2016-12-16 19:38:21 -0500

    Made the maps page

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef} from '@angular/core';
 import { NavController } from 'ionic-angular';
+declare var google;
 
 @Component({
   selector: 'page-map',
@@ -8,7 +8,29 @@ import { NavController } from 'ionic-angular';
 })
 export class MapPage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewDidEnter(){
+    this.loadMap();
+  }
+
+  loadMap(){
+
+    let latLng = new google.maps.LatLng(38.2137257,-45.099381);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 4,
+      mapTypeId: google.maps.MapTypeId.SATELLITE
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    console.log("Loaded correctly");
 
   }
 
